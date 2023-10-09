6f65df82ce55e9e8791708601f1588ac866b8e14
Simon Vetter <vetter@esa.tu-darmstadt.de>
2023-10-09 14:10:56 +0200

    Added GPL3 license

--- src/simetimer/TimeChunk.java
+++ src/simetimer/TimeChunk.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2015, 2020, 2023 Simon Vetter
+ *
+ * This file is part of SimeTimer.
+ *
+ * SimeTimer is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * SimeTimer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with SimeTimer.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package simetimer;
 
 import java.text.DateFormat;
